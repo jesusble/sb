d461ad706e6af0152723e5137aff3a5de42801d7
jesusble <keziahblessy33@gmail.com>
2019-03-06 13:56:24 +0530

    Create square.c

---
+++ square.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+ int n,t=0,s,p;
+ printf("Enter the number ");
+ scanf("%d",&n);
+ p=n;
+ do
+ {
+      r=q%10;
+      t=t+pow(s,2);
+      p=p/10;
+ }
+ while(p%10!=0);
+ printf("%d",t);
+ getch();
+}

9ea9824df9e00c16dec80f475f16190f38f05b64
jesusble <keziahblessy33@gmail.com>
2019-06-06 14:15:08 +0530

    Create smallest.py

---
+++ smallest.py
@@ -0,0 +1,12 @@
+def largest(arr,n): 
+    max = arr[0]
+    for i in range(1, n): 
+        if arr[i] < max: 
+            max = arr[i] 
+    return max
+arr =int(input())
+for i in range(1,arr):
+	arr1=int(input())
+	n = len(arr) 
+    Ans = largest(arr1,n) 
+    print ("Largest in given array is",Ans) 
